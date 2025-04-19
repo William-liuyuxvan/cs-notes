9e467e8bfed8c05aed6beaf4996b0a6c2f71dc5d
william-liuyuxvan <william1215liu@163.com>
2025-04-19 19:49:23 +0800

    chore: 了解了代码随想录算法和java学习路线，目前想跟着这个路线走，先学好java再学习go，然后每天刷题，并且看面经，然后学习java知识

---
+++ algorithm/Program_LeetCode/untitled/array/src/william/com/Test704.java
@@ -0,0 +1,27 @@
+package william.com;
+
+/**
+ * @ClassName Test704
+ * @Description TODO
+ * @Author eeekuu
+ * @Date 2025/4/19 19:04
+ */
+public class Test704 {
+    public int search(int[] nums, int target) {
+        if (nums.length == 0 || target > 9999 || target < -9999) return -1;
+        return searchByTwo(nums, 0, nums.length - 1, target);
+    }
+
+    public int searchByTwo(int[] nums, int left, int right, int target) {
+        if (left > right) return -1;
+
+        int mid = (right + left) / 2;
+        int temp = nums[mid];
+
+        if (target == temp) return mid;
+        else if (target > temp) return searchByTwo(nums, mid + 1, right, target);
+        else if (target < temp) return searchByTwo(nums, left, mid - 1, target);
+
+        return -1;
+    }
+}
