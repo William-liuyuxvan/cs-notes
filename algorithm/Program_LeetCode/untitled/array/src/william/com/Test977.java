e63aa35d0ad16b628f4d5aca157b4b79bd2587c4
william-liuyuxvan <william1215liu@163.com>
2025-04-20 21:44:02 +0800

    chore: 刷了移除元素和有序数组的平方题目，速学了html、css、js

---
+++ algorithm/Program_LeetCode/untitled/array/src/william/com/Test977.java
@@ -0,0 +1,28 @@
+package william.com;
+
+import java.util.Arrays;
+
+/**
+ * @ClassName Test977
+ * @Description TODO
+ * @Author eeekuu
+ * @Date 2025/4/20 14:29
+ */
+public class Test977 {
+    public int[] sortedSquares(int[] nums) {
+        int len = nums.length;
+        int[] res = new int[len];
+        for (int i = 0, j = len - 1, pos = len - 1; i <= j;) {
+            if ((nums[i] * nums[i]) > (nums[j] * nums[j])) {
+                res[pos] = nums[i] * nums[i];
+                ++i;
+            } else {
+                res[pos] = nums[j] * nums[j];
+                --j;
+            }
+            --pos;
+        }
+
+        return res;
+    }
+}
