82eca4cbd51087c1bd06a1d5a8e5eb3cf6bbb025
william-liuyuxvan <william1215liu@163.com>
2025-04-22 22:49:19 +0800

    chore: 写了螺旋矩阵三道题

---
+++ algorithm/Program_LeetCode/untitled/array/src/william/com/Test54.java
@@ -0,0 +1,67 @@
+package william.com;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName Test54
+ * @Description TODO
+ * @Author eeekuu
+ * @Date 2025/4/22 22:07
+ */
+public class Test54 {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        if (matrix.length == 0) return new ArrayList<>();
+
+        List<Integer> res = new ArrayList<>();
+
+        int i = 0, j = 0;
+        int cnt = 0;
+        int val = matrix.length;
+        int col = matrix[0].length;
+        int total = val * col;
+
+        boolean[][] flag = new boolean[val][col];
+
+        res.add(matrix[i][j]);
+        flag[i][j] = true;
+        cnt++;
+
+        while (cnt < total) {
+            while (cnt < total && j + 1 < col && !flag[i][j + 1]) { // 右
+                res.add(matrix[i][j + 1]);
+                flag[i][++j] = true;
+                cnt++;
+            }
+            while (cnt < total && i + 1 < val && !flag[i + 1][j]) { // 下
+                res.add(matrix[i + 1][j]);
+                flag[++i][j] = true;
+                cnt++;
+            }
+            while (cnt < total && j - 1 >= 0 && !flag[i][j - 1]) { // 左
+                res.add(matrix[i][j - 1]);
+                flag[i][--j] = true;
+                cnt++;
+            }
+            while (cnt < total && i - 1 >= 0 && !flag[i - 1][j]) { // 上
+                res.add(matrix[i - 1][j]);
+                flag[--i][j] = true;
+                cnt++;
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new Test54().spiralOrder(new int[][]{
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        });
+
+        for (Integer integer : list) {
+            System.out.print(integer + " ");
+        }
+    }
+}
